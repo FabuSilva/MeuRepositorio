459bd2f8fb60f60b5007e7070b69a4c0bc8b5d41
Fábio Silva <fabio.f3k@gmail.com>
2022-12-30 23:21:22 -0300

    começando a consumir API

---
+++ ProjetosWeb/CloneNetflix/public/js/api.js
@@ -0,0 +1,4 @@
+let api_key = "c30de842a0b4ba7a27abe67307c93adc"
+let img_url = "https://image.tmdb.org/t/p/w500"
+let geres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movie_geres_list = "https://api.themoviedb.org/3/discover/movie?"
