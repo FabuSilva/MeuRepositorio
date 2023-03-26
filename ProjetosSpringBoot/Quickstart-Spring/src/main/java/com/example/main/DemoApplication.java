143b125b742960fa1507b0caa602aa1f14a3a1a0
Fábio Silva <fabio.f3k@gmail.com>
2023-03-26 16:21:22 -0300

    add projetos

---
+++ ProjetosSpringBoot/Quickstart-Spring/src/main/java/com/example/main/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/home")
+	public String hello(@RequestParam(value = "name", defaultValue = "Fabu") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
