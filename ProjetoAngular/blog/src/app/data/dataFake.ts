ead81f817e30ed7b01423b5067ae94b3aa6d54be
Fábio Silva <fabio.f3k@gmail.com>
2023-10-18 20:11:07 -0300

    add project

---
+++ ProjetoAngular/blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "cardTitle": "Origem do Nome Angular",
+    "cardDescription": "Um fato curioso sobre o framework Angular é a origem do seu nome. Inicialmente, a equipe de desenvolvimento da Google o chamou de 'AngularJS', devido à ênfase na manipulação de 'ângulos' (ou seja, dados) nas aplicações da web. No entanto, quando o Angular foi redesenhado e reconstruído como Angular 2 e versões posteriores, o nome oficial foi simplificado para 'Angular'.",
+    "pictureCover":"https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/ANGULAR.png"
+  },
+  {
+    "id":"2",
+    "cardTitle": "Transforme Suas Ideias em Aplicações Web de Alto Desempenho",
+    "cardDescription": "bla blabla",
+    "pictureCover":"https://hermes.dio.me/articles/cover/86932ce6-6e4e-4184-a20e-9f26a997c740.png"
+  }
+]
