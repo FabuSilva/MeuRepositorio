646293dc984c86e1c1824dc378aa9d0b92b3ecb3
Fábio Silva <fabio.f3k@gmail.com>
2023-10-20 18:11:51 -0300

    add project

---
+++ ProjetosSpringBoot/apiRestComRailway/src/main/java/apiRestComRailway/ApiRestComRailwayApplication.java
@@ -0,0 +1,13 @@
+package apiRestComRailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestComRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestComRailwayApplication.class, args);
+	}
+
+}
---
+++ ProjetosSpringBoot/apiRestComRailway/src/test/java/apiRestComRailway/ApiRestComRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package apiRestComRailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestComRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
